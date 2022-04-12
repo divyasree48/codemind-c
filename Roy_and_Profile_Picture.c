b5eb57827e578f1bf09c561681d2ddf850b62dd4
Divya <divyasreem48@gmail.com>
2022-04-12 20:49:06 +0530

    #include<stdio.h> int main() {     int l,w,h,n,i;     scanf("%d",&l);     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d %d",&w,&h);         if(w<l||h<l)         {             printf("UPLOAD ANOTHER ");         }         else if((w>l||h>l) && w!=h)         {             printf("CROP IT ");         }         else if(w==h)         {             printf("ACCEPTED ");         }     }
    }

---
+++ Roy_and_Profile_Picture.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int l,w,h,n,i;
+    scanf("%d",&l);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d %d",&w,&h);
+        if(w<l||h<l)
+        {
+            printf("UPLOAD ANOTHER
+");
+        }
+        else if((w>l||h>l) && w!=h)
+        {
+            printf("CROP IT
+");
+        }
+        else if(w==h)
+        {
+            printf("ACCEPTED
+");
+        }
+    }
+    
+}
