1686bccbf30c109a649d10fddcd9e40bd03e0f36
Divya <divyasreem48@gmail.com>
2022-04-15 11:52:51 +0530

    #include<stdio.h> #include<string.h> int main() {     char str[100];     int i,c=1;     scanf("%[^ ]s",str);     for(i=1;str[i]!=NULL;i++)     {         if(str[i]>=65 && str[i]<=90)         {             c++;         }     }     printf("%d",c); }

---
+++ Camel_case_word_count.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100];
+    int i,c=1;
+    scanf("%[^
+]s",str);
+    for(i=1;str[i]!=NULL;i++)
+    {
+        if(str[i]>=65 && str[i]<=90)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
