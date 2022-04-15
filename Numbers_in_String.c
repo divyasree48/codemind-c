2ec61a0c0c4c2c8a91c97586001b889da7e9cd55
Divya <divyasreem48@gmail.com>
2022-04-15 11:30:15 +0530

    #include<stdio.h> #include<string.h> int main() {     char s[100];     int i,sum=0,len;     scanf("%[^ ]s",s);     len=strlen(s);     for(i=0;i<len;i++)     {         if(s[i]>='0' && s[i]<='9')         {             sum=sum+(s[i]-'0');         }     }     printf("%d",sum); }

---
+++ Numbers_in_String.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[100];
+    int i,sum=0,len;
+    scanf("%[^
+]s",s);
+    len=strlen(s);
+    for(i=0;i<len;i++)
+    {
+        if(s[i]>='0' && s[i]<='9')
+        {
+            sum=sum+(s[i]-'0');
+        }
+    }
+    printf("%d",sum);
+}
