0517e6a9a9d7c1afe261f0c886159533f58bf9ca
Divya <divyasreem48@gmail.com>
2022-04-16 06:22:51 +0530

    #include<stdio.h> int main() {     int arr[20],n,i,j,c=0,max=0,k=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(i!=j)             {                 if(arr[i]==arr[j])                 {                     c++;                     break;                 }             }         }         if(c==0)         {             if(arr[i]>max)             {                 max=arr[i];             }         }     }    if(max==0)    {        printf("-1");    }     else     {         printf("%d",max);     } }

---
+++ UNIQUE_MAXIMUM_NUMBER.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int arr[20],n,i,j,c=0,max=0,k=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(i!=j)
+            {
+                if(arr[i]==arr[j])
+                {
+                    c++;
+                    break;
+                }
+            }
+        }
+        if(c==0)
+        {
+            if(arr[i]>max)
+            {
+                max=arr[i];
+            }
+        }
+    }
+   if(max==0)
+   {
+       printf("-1");
+   }
+    else
+    {
+        printf("%d",max);
+    }
+}
