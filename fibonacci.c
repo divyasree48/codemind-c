dd4fb2652101d170f0547db8c9d83b71f2e9e715
Divya <divyasreem48@gmail.com>
2022-04-19 22:45:15 +0530

    #include<stdio.h> void fib(int n) {     int a,b,c,i;     a=0;b=1;c=a+b;     printf("%d %d ",a,b);     for(i=3;i<=n;i++)     {         printf("%d ",c);         a=b;b=c;c=a+b;     } } int main() {     int n;     scanf("%d",&n);     fib(n); }

--- fibonacci.c
+++ fibonacci.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
 void fib(int n)
 {
-    int a=0,b=1,c,i;
-    printf("%d %d",a,b);
-    c=a+b;
+    int a,b,c,i;
+    a=0;b=1;c=a+b;
+    printf("%d %d ",a,b);
     for(i=3;i<=n;i++)
     {
-        printf(" %d",c);
-        a=b;
-        b=c;
-        c=a+b;
+        printf("%d ",c);
+        a=b;b=c;c=a+b;
     }
 }
 int main()
