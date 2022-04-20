edd7132d7ebec8b762edeb09af3a58c73b1d87b9
Divya <divyasreem48@gmail.com>
2022-04-20 07:12:31 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,arr[100],o=0,e=0,d,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             o=o+arr[i];         }
        }
        
        printf("%d",o);
       
    }

---
+++ Even_Sum.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,arr[100],o=0,e=0,d,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            o=o+arr[i];
+        }
+       
+    }
+    
+    printf("%d",o);
+   
+}
