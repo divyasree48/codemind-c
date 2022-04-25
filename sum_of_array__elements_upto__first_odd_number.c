4a41487f67e686c6871bcecae9ec8829229258a5
Divya <divyasreem48@gmail.com>
2022-04-25 22:37:30 +0530

    #include<stdio.h> int main() {      int n,i,arr[10000],k,s=0,d=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             k=i;             d++;         }         if(d==1)         break;     }     for(i=0;i<k;i++)     {             s=s+arr[i];     }     printf("%d",s); }

---
+++ sum_of_array__elements_upto__first_odd_number.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+     int n,i,arr[10000],k,s=0,d=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            k=i;
+            d++;
+        }
+        if(d==1)
+        break;
+    }
+    for(i=0;i<k;i++)
+    {
+            s=s+arr[i];
+    }
+    printf("%d",s);
+}
