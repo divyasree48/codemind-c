e8e6d51a3100b0f8c6e84adc1f1ae10f0501ea14
Divya <divyasreem48@gmail.com>
2022-04-26 17:28:26 +0530

    #include<stdio.h> int fun(int *arr,int n) {     int i,k,r,rev=0,temp,d=0,rvs[100];     for(i=0,k=0;i<n;i++,k++)     {rev=0;         temp=arr[i];         while(arr[i])         {             r=arr[i]%10;
                arr[i]=arr[i]/10;
                rev=rev*10+r;
                rvs[k]=rev;
            }
            if(temp==rvs[k])
            {
                d++;
            
            }
        }
        return d;
    }
    int main()
    {
        int n,i,l,arr[100];
        scanf("%d",&n);
        for(i=0;i<n;i++)
        {
            scanf("%d",&arr[i]);
        }
        l=fun(arr,n);
        printf("%d",l);
    }

---
+++ Count_palindromes.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int fun(int *arr,int n)
+{
+    int i,k,r,rev=0,temp,d=0,rvs[100];
+    for(i=0,k=0;i<n;i++,k++)
+    {rev=0;
+        temp=arr[i];
+        while(arr[i])
+        {
+            r=arr[i]%10;
+            
+            arr[i]=arr[i]/10;
+            rev=rev*10+r;
+            rvs[k]=rev;
+        }
+        if(temp==rvs[k])
+        {
+            d++;
+        
+        }
+    }
+    return d;
+}
+int main()
+{
+    int n,i,l,arr[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    l=fun(arr,n);
+    printf("%d",l);
+}
