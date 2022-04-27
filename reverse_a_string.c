344f064717ff072e8ddd53cc2ca6cff2cf217c80
Divya <divyasreem48@gmail.com>
2022-04-27 22:31:46 +0530

    #include<stdio.h> #include<string.h> void stringrev(char *str) {     int i,j,len;     char temp;     len=strlen(str);     j=len-1;     while(i<j)     {         temp=str[i];         str[i]=str[j];         str[j]=temp;         i++;         j--;     }
    }
    int main()
    {
        char str[100];
        scanf("%[^
    ]",str);
        stringrev(str);
        printf("%s",str);
    }

---
+++ reverse_a_string.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+void stringrev(char *str)
+{
+    int i,j,len;
+    char temp;
+    len=strlen(str);
+    j=len-1;
+    while(i<j)
+    {
+        temp=str[i];
+        str[i]=str[j];
+        str[j]=temp;
+        i++;
+        j--;
+    }
+    
+}
+int main()
+{
+    char str[100];
+    scanf("%[^
+]",str);
+    stringrev(str);
+    printf("%s",str);
+}   
