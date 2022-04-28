8d059c21531a2e97879eb842be7f762295f4508a
Divya <divyasreem48@gmail.com>
2022-04-28 18:33:16 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],k,d;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     k=n-1;     d=k/2;     for(i=0;i<=k;i++,k--)     {         if(n%2!=0)         {             if(i!=d &&k!=d)                 {                      printf("%d %d ",arr[i],arr[k]);                 }
                if(i==d)
                    {
                        printf("%d ",arr[k]);
                        printf("0 ");
                    }
            
            if(i>d)
            {
                break;
            }
            }
            else
            {
                printf("%d %d ",arr[i],arr[k]);
            }
        }
    }

---
+++ First_and_Last_pair.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100],k,d;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    k=n-1;
+    d=k/2;
+    for(i=0;i<=k;i++,k--)
+    {
+        if(n%2!=0)
+        {
+            if(i!=d &&k!=d)
+                {
+                     printf("%d %d ",arr[i],arr[k]);
+                }
+        
+            if(i==d)
+                {
+                    printf("%d ",arr[k]);
+                    printf("0 ");
+                }
+        
+        if(i>d)
+        {
+            break;
+        }
+        }
+        else
+        {
+            printf("%d %d ",arr[i],arr[k]);
+        }
+    }
+}
