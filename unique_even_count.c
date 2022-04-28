0b43e6af6ee88229e9a8d094d8dc9ffd2fbff959
Divya <divyasreem48@gmail.com>
2022-04-28 21:50:59 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],s=0,j,k;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         for(j=i+1;j<n;j++)         {             if(arr[i]==arr[j])             {                 for(k=j;k<n-1;k++)                 {                     arr[k]=arr[k+1];                 }                 n--;                 j--;             }         }     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             s++;         }     }     printf("%d",s); }

---
+++ unique_even_count.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100],s=0,j,k;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                for(k=j;k<n-1;k++)
+                {
+                    arr[k]=arr[k+1];
+                }
+                n--;
+                j--;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            s++;
+        }
+    }
+    printf("%d",s);
+}
