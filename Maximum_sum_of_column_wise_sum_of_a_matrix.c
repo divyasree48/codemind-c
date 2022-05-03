03f50426ef3a5baca3d4eb8a6e81e681e694d76f
Divya <divyasreem48@gmail.com>
2022-05-03 22:03:18 +0530

    #include<stdio.h> int main() {     int i,j,r,c,arr[100][100],max=-999,s=0;     scanf("%d%d",&r,&c);     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&arr[i][j]);         }     }     for(j=0;j<c;j++)     {s=0;         for(i=0;i<r;i++)         {             s=s+arr[i][j];         }         if(s>max)         {             max=s;         }     }     printf("%d",max); }

---
+++ Maximum_sum_of_column_wise_sum_of_a_matrix.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,r,c,arr[100][100],max=-999,s=0;
+    scanf("%d%d",&r,&c);
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(j=0;j<c;j++)
+    {s=0;
+        for(i=0;i<r;i++)
+        {
+            s=s+arr[i][j];
+        }
+        if(s>max)
+        {
+            max=s;
+        }
+    }
+    printf("%d",max);
+}
