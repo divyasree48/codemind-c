171446a23e7aaad2a6a8df395282dd4b182b73df
Divya <divyasreem48@gmail.com>
2022-05-04 21:33:00 +0530

    #include<stdio.h> int main() {     int r,c,i,j,arr[1000][1000],c1=0,s1=0;     scanf("%d%d",&r,&c);     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&arr[i][j]);         }     }
        for(i=0;i<r;i++)
        {
            for(j=0;j<c-1;j++)
            {
                if(arr[i][j]>=arr[i][j+1])
                {
                    break;
                }
            }
            if(j==c-1)
            {
                s1++;
            }
        }
        for(i=0;i<r;i++)
        {
            for(j=c-1;j>0;j--)
            {
                if(arr[i][j]>=arr[i][j-1])
                {
                    break;
                }
            }
            if(c>1&&j==0)
            {
                s1++;
            }
        }
        
        printf("%d",s1);
    }

---
+++ Count_sorted_rows_in_a_matrix.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+    int r,c,i,j,arr[1000][1000],c1=0,s1=0;
+    scanf("%d%d",&r,&c);
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c-1;j++)
+        {
+            if(arr[i][j]>=arr[i][j+1])
+            {
+                break;
+            }
+        }
+        if(j==c-1)
+        {
+            s1++;
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+        for(j=c-1;j>0;j--)
+        {
+            if(arr[i][j]>=arr[i][j-1])
+            {
+                break;
+            }
+        }
+        if(c>1&&j==0)
+        {
+            s1++;
+        }
+    }
+    
+    printf("%d",s1);
+}
