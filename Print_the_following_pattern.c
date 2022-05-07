568d09e16ba80330f948b8652a20e096568b4e36
Divya <divyasreem48@gmail.com>
2022-05-07 18:36:43 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=n;j>=1;j--)         {             if(i==j)             {                 printf("%d ",i);             }             else if(i+j==n+1)             {                 printf("%d ",i);             }             else             {                 printf(" ");             }         }         printf(" ");     } }

---
+++ Print_the_following_pattern.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(j=n;j>=1;j--)
+        {
+            if(i==j)
+            {
+                printf("%d ",i);
+            }
+            else if(i+j==n+1)
+            {
+                printf("%d ",i);
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("
+");
+    }
+}
