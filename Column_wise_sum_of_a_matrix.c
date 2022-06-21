85ec90c2e771321d4cbdc527b06e2007a4320371
Divya <divyasreem48@gmail.com>
2022-06-21 14:13:01 +0530

    #include<stdio.h> int main() {     int r,c,i,j,arr[100][100],s=0,s1=0,max=-999,max1=-999;     scanf("%d%d",&r,&c);     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&arr[i][j]);         }     }    for(j=0;j<c;j++)    {        s=0;        for(i=0;i<r;i++)        {            s+=arr[i][j];        }       printf("%d ",s);    }
    }

--- Column_wise_sum_of_a_matrix.c
+++ Column_wise_sum_of_a_matrix.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int r,c,i,j,arr[100][100],d=0;
+    int r,c,i,j,arr[100][100],s=0,s1=0,max=-999,max1=-999;
     scanf("%d%d",&r,&c);
     for(i=0;i<r;i++)
     {
@@ -10,12 +10,14 @@ int main()
             scanf("%d",&arr[i][j]);
         }
     }
-    for(i=0;i<c;i++)
-    {d=0;
-        for(j=0;j<r;j++)
-        {
-            d+=arr[j][i];
-        }
-        printf("%d ",d);
-    }
+   for(j=0;j<c;j++)
+   {
+       s=0;
+       for(i=0;i<r;i++)
+       {
+           s+=arr[i][j];
+       }
+      printf("%d ",s);
+   }
+   
 }
