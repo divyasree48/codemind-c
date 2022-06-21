e898e979b156c1b1a1d9f2b7baede40fdae4ff47
Divya <divyasreem48@gmail.com>
2022-06-21 14:04:21 +0530

    #include<stdio.h> int main() {     int r,c,i,j,arr[100][100],s=0,s1=0,max=-999,max1=-999;     scanf("%d%d",&r,&c);     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&arr[i][j]);         }     }    for(j=0;j<c;j++)    {        s=0;        for(i=0;i<r;i++)        {            s+=arr[i][j];        }        if(s>max)        {            max=s;        }    }    for(i=0;i<r;i++)    {s1=0;        for(j=0;j<c;j++)        {            s1+=arr[i][j];        }        if(s1>max1)        {            max1=s1;        }    }    if(max>max1)    {        printf("%d",max);    }    else    printf("%d",max1); }

--- Maximum_sum_of_row_wise_sum_or_column_wise_sum_of_matrix.c
+++ Maximum_sum_of_row_wise_sum_or_column_wise_sum_of_matrix.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,r,c,arr[1000][1000],max=-9999,s=0,s1=0,max1=-999;
+    int r,c,i,j,arr[100][100],s=0,s1=0,max=-999,max1=-999;
     scanf("%d%d",&r,&c);
     for(i=0;i<r;i++)
     {
@@ -10,30 +10,33 @@ int main()
             scanf("%d",&arr[i][j]);
         }
     }
-    for(j=0;j<c;j++)
-    {s=0;
-        for(i=0;i<r;i++)
-        {
-            s=s+arr[i][j];
-        }
-        if(s>max)
-        {
-            max=s;
-        }
-    }
-    for(i=0;i<r;i++)
-    {s1=0;
-        for(j=0;j<c;j++)
-        {
-            s1=s1+arr[i][j];
-        }
-        if(s1>max1)
-        {
-            max1=s1;
-        }
-    }
-    if(max>max1)
-    printf("%d",max);
-    else 
-    printf("%d",max1);
+   for(j=0;j<c;j++)
+   {
+       s=0;
+       for(i=0;i<r;i++)
+       {
+           s+=arr[i][j];
+       }
+       if(s>max)
+       {
+           max=s;
+       }
+   }
+   for(i=0;i<r;i++)
+   {s1=0;
+       for(j=0;j<c;j++)
+       {
+           s1+=arr[i][j];
+       }
+       if(s1>max1)
+       {
+           max1=s1;
+       }
+   }
+   if(max>max1)
+   {
+       printf("%d",max);
+   }
+   else
+   printf("%d",max1);
 }
