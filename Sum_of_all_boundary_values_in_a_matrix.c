559e5ac230ccac007304844e781953ff0f9ec8c1
Divya <divyasreem48@gmail.com>
2022-06-21 13:54:11 +0530

    #include<stdio.h> int main() {     int r,c,i,j,arr[100][100],s=0,s1=0;     scanf("%d%d",&r,&c);     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&arr[i][j]);         }     }
        for(i=0;i<r;i++)
        {
            for(j=0;j<c;j++)
            {
               
                    s+=arr[i][j];
                
            }
        }
        
        for(i=0;i<r;i++)
        {
            for(j=0;j<c;j++)
            {
                if(i==0 || j==0 || i==r-1 || j==c-1)
                {
                    s1+=arr[i][j];
                }
            }
        }
        printf("%d",s1);
    }

--- Sum_of_all_boundary_values_in_a_matrix.c
+++ Sum_of_all_boundary_values_in_a_matrix.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int r,c,i,j,arr[1000][1000],s=0,s1=0;
+    int r,c,i,j,arr[100][100],s=0,s1=0;
     scanf("%d%d",&r,&c);
     for(i=0;i<r;i++)
     {
@@ -10,20 +10,26 @@ int main()
             scanf("%d",&arr[i][j]);
         }
     }
-    
    
     for(i=0;i<r;i++)
     {
         for(j=0;j<c;j++)
         {
-            if(i==0 || j==0 ||i==r-1 || j==c-1)
-            {
-                s1=s1+arr[i][j];
-            }
+           
+                s+=arr[i][j];
+            
         }
     }
     
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            if(i==0 || j==0 || i==r-1 || j==c-1)
+            {
+                s1+=arr[i][j];
+            }
+        }
+    }
     printf("%d",s1);
-    
-    
 }
