a26e941d23a35dfe0119826f753662e9982619f1
Divya <divyasreem48@gmail.com>
2022-06-21 08:03:04 +0530

    #include<stdio.h> int main() {     int r,c,i,j,arr[100][100],s=0,s1=0,brr[100][100],d=0;     scanf("%d%d",&r,&c);     for(i=0;i<r;i++)     {         for (j=0;j<c;j++)         {         scanf("%d",&arr[i][j]);         }     }     for(i=0;i<r;i++)     {         for (j=0;j<c;j++)         {             if (arr[i][j]%2==0)             {                 s1+=arr[i][j];
                }
            }
        }
        //printf("%d ",s1);
        for(i=0;i<r;i++)
        {
            for (j=0;j<c;j++)
            {
                if (arr[i][j]%2!=0)
                {
                    s+=arr[i][j];
                    
                }
            }
        }
        printf("%d",s+s1);
    }

--- Sum_of_all_elements_in_a_matrix.c
+++ Sum_of_all_elements_in_a_matrix.c
@@ -1,22 +1,37 @@
 #include<stdio.h>
 int main()
 {
-    int r,c,i,j,arr[100][100],d=0;
+    int r,c,i,j,arr[100][100],s=0,s1=0,brr[100][100],d=0;
     scanf("%d%d",&r,&c);
     for(i=0;i<r;i++)
     {
-        for(j=0;j<c;j++)
+        for (j=0;j<c;j++)
         {
-            scanf("%d",&arr[i][j]);
+        scanf("%d",&arr[i][j]);
         }
     }
-    for(i=0;i<c;i++)
+    for(i=0;i<r;i++)
     {
-        for(j=0;j<r;j++)
+        for (j=0;j<c;j++)
         {
-            d+=arr[j][i];
+            if (arr[i][j]%2==0)
+            {
+                s1+=arr[i][j];
+                
+            }
         }
     }
-        printf("%d ",d);
-    
+    //printf("%d ",s1);
+    for(i=0;i<r;i++)
+    {
+        for (j=0;j<c;j++)
+        {
+            if (arr[i][j]%2!=0)
+            {
+                s+=arr[i][j];
+                
+            }
+        }
+    }
+    printf("%d",s+s1);
 }
