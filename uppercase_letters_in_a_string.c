bf33d2aafa5d041f31701cba77b471ed5955179d
Divya <divyasreem48@gmail.com>
2022-06-21 19:46:53 +0530

    #include<stdio.h> #include<string.h> int main() {  int i,l,c=0;  char s[100];  scanf("%[^ ]",s);  for(i=0;s[i]!=NULL;i++)  {      if (s[i]>='A'&&s[i]<='Z')      {          c++;      }  }  printf("%d",c); }

--- uppercase_letters_in_a_string.c
+++ uppercase_letters_in_a_string.c
@@ -2,16 +2,16 @@
 #include<string.h>
 int main()
 {
-    int i,l,c=0;
-    char s[100];
-    scanf("%[^
-]s",s);
-    for(i=0;s[i]!=NULL;i++)
-    {
-        if(s[i]>='A' && s[i]<='Z')
-        {
-            c++;
-        }
-    }
-    printf("%d",c);
+ int i,l,c=0;
+ char s[100];
+ scanf("%[^
+]",s);
+ for(i=0;s[i]!=NULL;i++)
+ {
+     if (s[i]>='A'&&s[i]<='Z')
+     {
+         c++;
+     }
+ }
+ printf("%d",c);
 }
