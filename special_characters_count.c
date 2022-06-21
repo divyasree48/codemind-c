138afeafe0eea208bfc102eee9aca4807a5c2af9
Divya <divyasreem48@gmail.com>
2022-06-21 19:14:45 +0530

    #include<stdio.h> #include<string.h> int main() {     int w=0,k=0,i,l;     char str[10000];     scanf("%[^ ]",str);     l=strlen(str);     for(i=0;i<l;i++)     {         if((str[i]>=97 && str[i]<=122)||(str[i]>=65 && str[i]<=90))         w++;         if(str[i]==' ')         k++;
        }
        printf("%d",l-k-w);
    }

--- special_characters_count.c
+++ special_characters_count.c
@@ -5,18 +5,15 @@ int main()
     int w=0,k=0,i,l;
     char str[10000];
     scanf("%[^
-]s",str);
+]",str);
     l=strlen(str);
     for(i=0;i<l;i++)
     {
         if((str[i]>=97 && str[i]<=122)||(str[i]>=65 && str[i]<=90))
-        {
-            w++;
-        }
+        w++;
         if(str[i]==' ')
-        {
-            k++;
-        }
+        k++;
+        
     }
     printf("%d",l-k-w);
 }
