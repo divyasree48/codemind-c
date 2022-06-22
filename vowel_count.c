613dcb527b8ee3e2ceae458aac891182ca4dbaf4
Divya <divyasreem48@gmail.com>
2022-06-22 06:59:48 +0530

    #include<stdio.h> #include<string.h> int main() {     int i,l,c=0;     char s[100];     scanf("%[^ ]",s);     l=strlen(s);     for(i=0;i<l;i++)     {         if (s[i]=='A' || s[i]=='E'|| s[i]=='I'|| s[i]=='O'|| s[i]=='U'|| s[i]=='a'|| s[i]=='e'|| s[i]=='i'|| s[i]=='o'|| s[i]=='u')         c++;     }     printf("%d",c); }

--- vowel_count.c
+++ vowel_count.c
@@ -2,17 +2,15 @@
 #include<string.h>
 int main()
 {
-int i,l,c=0;
-char s[100];
-scanf("%[^
+    int i,l,c=0;
+    char s[100];
+    scanf("%[^
 ]",s);
-l=strlen(s);
-for(i=0;i<l;i++)
-{
-    if(s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'||s[i]=='u'||s[i]=='o'||s[i]=='i'||s[i]=='e'||s[i]=='a')
+    l=strlen(s);
+    for(i=0;i<l;i++)
     {
+        if (s[i]=='A' || s[i]=='E'|| s[i]=='I'|| s[i]=='O'|| s[i]=='U'|| s[i]=='a'|| s[i]=='e'|| s[i]=='i'|| s[i]=='o'|| s[i]=='u')
         c++;
     }
-}
-printf("%d",c);
+    printf("%d",c);
 }
