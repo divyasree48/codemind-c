d183b0b1fceafaf19413eb93313e415692061184
Divya <divyasreem48@gmail.com>
2022-06-23 06:54:41 +0530

    #include<stdio.h> void fun(int *arr,int n) {     int i,j,rvs[100],r,rev=0;     for(i=0;i<n;i++)     {         rev=0;         while(arr[i])         {             r=arr[i]%10;             arr[i]=arr[i]/10;             rev=rev*10+r;             rvs[i]=rev;         }
        }
        for(i=0;i<n;i++)
        {
            printf("%d ",rvs[i]);
        }
        
    }
    int main()
    {
        int n,i,arr[100];
        scanf("%d",&n);
        for(i=0;i<n;i++)
        {
            scanf("%d",&arr[i]);
        }
        fun(arr,n);
    }

--- Reverse_game.c
+++ Reverse_game.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 void fun(int *arr,int n)
 {
-    int i,j,r,rev=0,rvs[100];
-    
+    int i,j,rvs[100],r,rev=0;
     for(i=0;i<n;i++)
-    {rev=0;
+    {
+        rev=0;
         while(arr[i])
         {
             r=arr[i]%10;
@@ -12,6 +12,7 @@ void fun(int *arr,int n)
             rev=rev*10+r;
             rvs[i]=rev;
         }
+        
     }
     for(i=0;i<n;i++)
     {
@@ -21,13 +22,11 @@ void fun(int *arr,int n)
 }
 int main()
 {
-     int n,i,arr[10000],k;
+    int n,i,arr[100];
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    
     fun(arr,n);
-    
 }
