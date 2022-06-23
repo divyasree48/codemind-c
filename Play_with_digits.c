0a132acd6660990b953ffde93582634619dfeb30
Divya <divyasreem48@gmail.com>
2022-06-23 06:59:54 +0530

    #include<stdio.h> int fun(int *arr,int n) {     int s=0,r,i;     for(i=0;i<n;i++)     {         while(arr[i])         {         r=arr[i]%10;         arr[i]=arr[i]/10;         s+=r;         }     }    printf("%d",s); } int main() {     int n,i,l,arr[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     l=fun(arr,n);
    }

--- Play_with_digits.c
+++ Play_with_digits.c
@@ -1,25 +1,26 @@
 #include<stdio.h>
-void fun(int *arr,int n)
+int fun(int *arr,int n)
 {
-    int i,s=0,r;
+    int s=0,r,i;
     for(i=0;i<n;i++)
     {
         while(arr[i])
         {
-            r=arr[i]%10;
-            arr[i]=arr[i]/10;
-            s=s+r;
+        r=arr[i]%10;
+        arr[i]=arr[i]/10;
+        s+=r;
         }
     }
-    printf("%d",s);
+   printf("%d",s);
 }
 int main()
 {
-    int i,n,arr[100];
+    int n,i,l,arr[100];
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-   fun(arr,n);
+    l=fun(arr,n);
+   
 }
